8a2d34b1f96c31876766e4934d06e0bfeb5d935e
nenad-dram <nenaddramicanin@gmail.com>
2022-05-26 20:49:11 +0200

    Add model classes and MongoDB configuration

---
+++ src/main/java/com/endyary/springdatamongodb/model/Order.java
@@ -0,0 +1,107 @@
+package com.endyary.springdatamongodb.model;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "orders")
+public class Order {
+
+    @Id
+    private Long id;
+
+    @Field
+    private Customer customer;
+
+    @Field
+    private List<Item> items;
+
+    @Field
+    private Status status;
+
+    @Field
+    private LocalDateTime createdDate;
+
+    @Field
+    private LocalDateTime modifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public enum Status {
+        PENDING,
+        SHIPPED,
+        DELIVERED
+    }
+
+    public static class Item {
+        private Product product;
+
+        private int quantity;
+
+        public Product getProduct() {
+            return product;
+        }
+
+        public void setProduct(Product product) {
+            this.product = product;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+}
---
+++ src/main/java/com/endyary/springdatamongodb/model/Product.java
@@ -0,0 +1,69 @@
+package com.endyary.springdatamongodb.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    private Long id;
+
+    @Field
+    private String name;
+
+    @Field
+    private String description;
+
+    @Field
+    private float price;
+
+    @Field
+    private Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public enum Status {
+        AVAILABLE,
+        UNAVAILABLE
+    }
+}
